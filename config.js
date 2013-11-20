1e91e2574ef0e18e3095c60f09bb4cd9a40f0f40
David Crowell <david@intrepid.io>
2013-11-19 22:55:07 -0500

    Heroku also is alone in receiving the config file

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.echonestApiKey = "BOAYYST4VLXT0J6UC";
+exports.similarUrl = "http://developer.echonest.com/api/v4/artist/similar?api_key=BOAYYST4VLXT0J6UC&format=json&results=1&start=0&name="; //name=Radiohead || id=
+exports.suggestUrl = "http://developer.echonest.com/api/v4/artist/suggest?api_key=BOAYYST4VLXT0J6UC&results=10&name=";
