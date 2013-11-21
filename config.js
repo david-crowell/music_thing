45b963bc719582cb7627a6022f391060ad9eeb4b
dcrowell <david@intrepid.io>
2013-11-21 12:02:44 -0500

    Modified config

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 exports.echonestApiKey = "BOAYYST4VLXT0J6UC";
+exports.seatgeekClientId = "NDUwNjA3fDEzODQ5NjE5MDY"
 exports.similarUrl = "http://developer.echonest.com/api/v4/artist/similar?api_key=BOAYYST4VLXT0J6UC&format=json&results=1&start=0&name="; //name=Radiohead || id=
 exports.suggestUrl = "http://developer.echonest.com/api/v4/artist/suggest?api_key=BOAYYST4VLXT0J6UC&results=10&name=";
