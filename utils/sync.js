7f6df61b9849bbd83dc634ff2ce9f58acd4ed188
dcrowell <david@intrepid.io>
2014-06-01 03:48:34 -0400

    Now pedigogically pedantic - async stuff is fun

--- utils/sync.js
+++ utils/sync.js
@@ -36,15 +36,52 @@ function parallelSaveResults(callback, error, functions, nonCallbackErrorArgumen
 	return;
 }
 
-// Doing in parallel means we can't guarantee the order of callbackArg results!
+/* You can match the index of the function in your input with the key in the argument to your callback containing
+ *   the argument each of your functions passed to its callback
+ *
+ * Contract: 
+ *   - each member of funcitons takes at least two arguments, arg0 = callback, arg1 = error
+ *   - the members of each array in nonCallbackErrorArguments will be passed to the appropriate function as arg2, arg3, etc.
+ *   - the arguments each member of functions passes to its callbacks will be returned in parallelSaveCallbackArgs's callback
+ *     - that callback will get one argument, a dictionary mapping 
+ *       - key = (a string of) the index of the function that generated these callback arguments
+ * 		 - value = a dictionary mapping (a string of) the arg index to the arg
+ *
+ * Example:
+ *   parallelSaveCallbackArgs(  
+ *	 	yourCallback, 
+ * 		yourError, 
+ *		[
+ *			function(callback, error, val){
+ *				callback(Math.ceil(val));
+ *			},
+ *			function(callback, error, a, b){
+ *				callback(Math.pow(a,b));
+ *			}
+ *		], 
+ *		[
+ *			[.1], 
+ *			[3,2]
+ *		] 
+ *	);
+ *	 runs the Match.ceil(.1) wrapper and Math.pow(3,2) wrapper in as close to parallel as node has
+ *   you get back:
+ *	{
+ *		"0": ["0":1],
+ *	 	"1": ["0":9]
+ *	 }
+ */
+
 function parallelSaveCallbackArgs(callback, error, functions, nonCallbackErrorArguments) {
 	var completed = 0;
 	var toDo = functions.length;
 	var callbackArgs = [];
 
-	function done() {
+	function done(i, args) {
 		completed += 1;
-		callbackArgs.push(arguments);
+
+		var callbackArg = {}; callbackArg[i] = args; callbackArgs.push(callbackArg);
+
 		if (completed == toDo) {
 			console.log("completed == toDo");
 			callback(callbackArgs);
@@ -52,9 +89,15 @@ function parallelSaveCallbackArgs(callback, error, functions, nonCallbackErrorAr
 	}
 
 	for (var i = 0; i < functions.length; i++) {
-		var arguments = [done, done].concat(nonCallbackErrorArguments[i]);
-		//console.log(arguments);
-		functions[i].apply(this, arguments);
+		// sort of silly closure to trap the i so we can identifiy which function each callbackArgs came from
+		function runIdentifiedFunctionWithNonCallbackErrorArgs(i, func, otherArgs) {
+			function doneWithI() {
+				done([i],arguments);
+			}
+			var args = [doneWithI, doneWithI].concat(otherArgs);
+			func.apply(this, args);
+		}
+		runIdentifiedFunctionWithNonCallbackErrorArgs(i,functions[i],nonCallbackErrorArguments[i]);
 	}
 	return;
 }
@@ -77,7 +120,7 @@ function test(){
 
 	function connect4(callback, error, a, b, c, d) {
 		console.log("connect4");
-		callback(a + ":" + b + "::" + c + ":" + d);
+		callback(a + ":" + b + "::" + c + ":" + d, a, b, c, d);
 	}
 
 	parallelSaveCallbackArgs(
